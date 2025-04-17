6a6343742c65e2907859cd90e5b9e5319563748e
Arnav Mishra <acromatic121@gmail.com>
2025-04-17 20:37:06 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
+// script.js
 
+document.addEventListener('DOMContentLoaded', () => {
+  console.log("Cyber Mind Space website loaded!");
+
+  // Future: Add scroll animation or modal if needed
+});
